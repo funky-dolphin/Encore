21ec105a8467890d8af9b3ab46c6ae931ea6da17
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-06-02 17:35:46 -0400

    sessions

--- server/models.py
+++ server/models.py
@@ -5,9 +5,6 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from config import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-
-
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
