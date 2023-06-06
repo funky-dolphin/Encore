7dc6daa0eaba972d492ea25af7d80c6005de1741
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-06-05 22:09:46 -0400

    edit

--- server/config.py
+++ server/config.py
@@ -4,32 +4,28 @@ from sqlalchemy import MetaData
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_session import Session
-# from flask_hashing import Hashing
-
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-
-db = SQLAlchemy(metadata = metadata) 
-
 app = Flask(__name__)
 CORS(app)
 
-
-
-app.config['SECRET_KEY'] = 'encore'
+app.config['SECRET_KEY']='encore'
 app.config['SESSION_COOKIE_NAME'] = 'encore_cookie'
 app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'  # add this line
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_TYPE'] = 'filesystem'  # add this line
 app.json.compact = False
-app.config['SESSION_TYPE'] = 'filesystem' 
-app.config['SESSION_PERMANENT'] = True
 
+db = SQLAlchemy(metadata = metadata, app=app)
 migrate = Migrate(app, db)
-db.init_app(app)
+
+
+
 
 
 
